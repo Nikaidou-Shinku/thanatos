214ec50c479e96c5a2ee9ab59bf35b8e62145910
yurzhang <yurzhang.oi@gmail.com>
2025-05-08 04:59:03 +0800

    feat: graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,33 @@ async fn main() -> Result<()> {
 
   tracing::info!(%addr, "Listening...");
 
-  axum::serve(listener, app).await?;
+  axum::serve(listener, app)
+    .with_graceful_shutdown(shutdown_signal())
+    .await?;
 
   Ok(())
+}
+
+async fn shutdown_signal() {
+  let ctrl_c = async {
+    tokio::signal::ctrl_c()
+      .await
+      .expect("failed to install Ctrl+C handler");
+  };
+
+  #[cfg(unix)]
+  let terminate = async {
+    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+      .expect("failed to install signal handler")
+      .recv()
+      .await;
+  };
+
+  #[cfg(not(unix))]
+  let terminate = std::future::pending();
+
+  tokio::select! {
+    _ = ctrl_c => { }
+    _ = terminate => { }
+  }
 }
